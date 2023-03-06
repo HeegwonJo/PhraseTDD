cee749a8ef2de1cdb60b33ef71575770a77c127f
Joyfulgwon <101499795+HeegwonJo@users.noreply.github.com>
2023-03-06 15:30:31 +0900

    등록 까지 구현 (#2)
    확인

--- phraseTDD/src/test/java/AppTests.java
+++ phraseTDD/src/test/java/AppTests.java
@@ -72,26 +72,6 @@ public class AppTests {
                 .contains("1번 명언이 등록되었습니다.");
     }
 
-    @Test
-    @DisplayName("명언이 등록될 때 마다 생성되는 명언의 번호가 1씩 증가한다.")
-    public void t6() {
-        String rs = AppTestRunner.run("""
-                등록
-                현재를 사랑하라.
-                작자미상
-                등록
-                나의 죽음을 적들에게 알리지 마라.
-                이순신
-                등록
-                왼손은 거들뿐
-                강백호
-                """);
 
-        assertThat(rs)
-                .contains("1번 명언이 등록되었습니다.")
-                .contains("2번 명언이 등록되었습니다.")
-                .contains("3번 명언이 등록되었습니다.")
-                .doesNotContain("4번 명언이 등록되었습니다.");
-    }
     // 앱 테스트 끝
 }
